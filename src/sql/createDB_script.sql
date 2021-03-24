f6229306bd60109ad0bd2ef80a2460d8327e3c4c
Haizea Rodriguez <haizearodriguez@opendeusto.es>
2021-03-24 22:24:02 +0100

    restart

--- src/sql/createDB_script.sql
+++
@@ -1,8 +0,0 @@
--- CREATE TABLES --
-CREATE TABLE donaciones (
-	Fecha VARCHAR(10) NOT NULL, 
-	Hora VARCHAR(8) NOT NULL, 
-	Donativo INTEGER(3) NOT NULL, 
-	Acumulado INTEGER NOT NULL,
-	PRIMARY KEY(Fecha, Hora, Acumulado) 
-);
