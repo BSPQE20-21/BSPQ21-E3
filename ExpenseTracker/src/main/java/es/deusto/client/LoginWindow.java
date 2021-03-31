9cf4aab44e8597feab5884e4987172e490ad9654
Haizea Rodriguez <haizearodriguez@opendeusto.es>
2021-03-31 11:17:36 +0200

    Added a default window #BSPQ21E3-21

---
+++ ExpenseTracker/src/main/java/es/deusto/client/LoginWindow.java
@@ -0,0 +1,85 @@
+package es.deusto.client;
+
+// We need to change it, I just copy paste it from the internet so we can use it as a base
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class LoginWindow extends JFrame implements ActionListener {
+
+    JPanel panel;
+    JLabel user_label, password_label, message;
+    JTextField userName_text;
+    JPasswordField password_text;
+    JButton submit, cancel;
+
+    LoginWindow() {
+        
+        // User Label
+        user_label = new JLabel();
+        user_label.setText("User Name :");
+        userName_text = new JTextField();
+        
+        // Password
+
+        password_label = new JLabel();
+        password_label.setText("Password :");
+        password_text = new JPasswordField();
+
+        // Submit
+
+        submit = new JButton("SUBMIT");
+
+        panel = new JPanel(new GridLayout(3, 1));
+
+        panel.add(user_label);
+        panel.add(userName_text);
+        panel.add(password_label);
+        panel.add(password_text);
+
+        message = new JLabel();
+        panel.add(message);
+        panel.add(submit);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Adding the listeners to components..
+        submit.addActionListener(this);
+        add(panel, BorderLayout.CENTER);
+        setTitle("Please Login Here !");
+        setSize(300, 100);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new LoginWindow();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        String userName = userName_text.getText();
+        String password = password_text.getText();
+        // this is hardcoded we cannot hardcode. 
+        // we will need to recieve this info from the server side
+        if (userName.trim().equals("admin") && password.trim().equals("admin")) {
+            message.setText(" Hello " + userName
+                    + "");
+        } else {
+            message.setText(" Invalid user.. ");
+        }
+
+    }
+
+}
