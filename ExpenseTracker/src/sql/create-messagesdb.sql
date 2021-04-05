3f769696a3ebb06bb1306f55382a495def03f5d5
Haizea Rodriguez <haizearodriguez@opendeusto.es>
2021-04-05 10:51:36 +0200

    db schema edited

--- ExpenseTracker/src/sql/create-messagesdb.sql
+++ ExpenseTracker/src/sql/create-messagesdb.sql
@@ -1,11 +1,11 @@
 /* DELETE 'messagesDB' database*/
-DROP SCHEMA IF EXISTS messagesDB;
+DROP SCHEMA IF EXISTS ExpensesDB;
 /* DELETE USER 'spq' AT LOCAL SERVER*/
 DROP USER IF EXISTS 'spq'@'localhost';
 
 /* CREATE 'messagesDB' DATABASE */
-CREATE SCHEMA messagesDB;
+CREATE SCHEMA ExpensesDB;
 /* CREATE THE USER 'spq' AT LOCAL SERVER WITH PASSWORD 'spq' */
 CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
 
-GRANT ALL ON messagesDB.* TO 'spq'@'localhost';
+GRANT ALL ON ExpensesDB.* TO 'spq'@'localhost';
