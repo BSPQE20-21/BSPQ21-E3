9cdf887470d54198fdad4c93819b529167f120c8
mireyaquintana <47787650+mireyaquintana@users.noreply.github.com>
2021-04-06 11:57:07 +0200

    Create registration window and correct storeExpense method

--- ExpenseTracker/src/main/java/es/deusto/client/ExampleClient.java
+++ ExpenseTracker/src/main/java/es/deusto/client/ExampleClient.java
@@ -19,6 +19,7 @@ import es.deusto.serialization.ExpenseData;
 import es.deusto.serialization.UserData;
 import es.deusto.server.jdo.Category;
 import es.deusto.server.jdo.Expense;
+import es.deusto.server.jdo.User;
 
 // This class is the client side of the arquitecture. There is nothing else on this side but we are going to need:
 // GUI - windows
@@ -33,88 +34,19 @@ import es.deusto.server.jdo.Expense;
 // login 
 // include expense
 
-public class ExampleClient implements ActionListener, Runnable  {
-	
-	private JFrame frame; 
-	private JButton buttonRegister, buttonEnd; 
-	private JTextField emailField, carNumberField, ageField, expenseLimitField;  
-	private JPasswordField passwordField; 
-	private Thread thread;
-	private final AtomicBoolean running = new AtomicBoolean(false);
+public class ExampleClient {
 
 	private Client client;
 	private WebTarget webTarget;
+	private RegisterWindow rw;
 
 	public ExampleClient(String hostname, String port) {
 		client = ClientBuilder.newClient();
 		webTarget = client.target(String.format("http://%s:%s/rest/server", hostname, port));
-	
-		JPanel registerPanel = new JPanel();
-		registerPanel.setLayout(new BoxLayout(registerPanel, BoxLayout.Y_AXIS));
-		// EMAIL 
-		registerPanel.add(new JLabel("Email: "));
-		this.emailField = new JTextField(20);
-		registerPanel.add(this.emailField);
-		// PASSWORD 
-		registerPanel.add(new JLabel("Password: "));
-		this.passwordField = new JPasswordField(10);
-		registerPanel.add(this.passwordField);
-		// CARD NUMBER
-		registerPanel.add(new JLabel("Card Number: "));
-		this.carNumberField = new JTextField(10);
-		registerPanel.add(this.carNumberField);
-		// AGE
-		registerPanel.add(new JLabel("Age: "));
-		this.ageField = new JTextField(10);
-		registerPanel.add(this.ageField);
-		// EXPENSE LIMIT
-		registerPanel.add(new JLabel("Expense Limit: ")); 
-		this.expenseLimitField = new JTextField(10);
-		registerPanel.add(this.expenseLimitField);
-
-
-		this.buttonRegister = new JButton("Register");
-		registerPanel.add(this.buttonRegister); 
-		this.buttonEnd = new JButton("End");
-		registerPanel.add(this.buttonEnd); 
-		buttonRegister.addActionListener(this);
-
-		this.frame = new JFrame("Registration"); 
-		this.frame.setTitle("REGISTRATION");
-
-		this.frame.setSize(600,500);
-		this.frame.getContentPane().add(registerPanel,"North"); 
-
-		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//WindowManager.middleLeft(this.frame); Is a class of the project
-		this.frame.setVisible(true);
-
-		thread = new Thread(this);
-		thread.start();
-	
-	
+		rw = new RegisterWindow(this);
 	}
 
-	public void actionPerformed(ActionEvent e) {
-		JButton target = (JButton) e.getSource();
-		if (target == this.buttonEnd) {
-			this.stop();
-			System.exit(0);
-		}
-		if (target == this.buttonRegister) {
-			try {
-				System.out.println("entra");
-				String login = this.emailField.getText(); 
-				String password = String.valueOf(this.passwordField.getPassword()); 
-				String cardNumber = this.carNumberField.getText(); 
-				int age = Integer.parseInt(this.ageField.getText());
-				double expenseLimit = Double.parseDouble(this.expenseLimitField.getText()); 
-				this.registerUser(login, password, cardNumber, age, expenseLimit);
-			} catch (NumberFormatException exc) {
-				// TODO 
-			}
-		}
-	}
+	
 	public void registerUser(String login, String password, String cardNumber, int age, double expenseLimit) {
 		WebTarget registerUserWebTarget = webTarget.path("register");
 		Invocation.Builder invocationBuilder = registerUserWebTarget.request(MediaType.APPLICATION_JSON);
@@ -184,21 +116,18 @@ public class ExampleClient implements ActionListener, Runnable  {
 		}
 	}
 	
-	public void storeExpense(String login, String password, Expense expense) {
-		WebTarget storeExpenseWebTarget = webTarget.path("register");
+	public void storeExpense(UserData userData, String text, double amount, Category category) {
+		WebTarget storeExpenseWebTarget = webTarget.path("store");
 		Invocation.Builder invocationBuilder = storeExpenseWebTarget.request(MediaType.APPLICATION_JSON);
 		
 		DirectedMessage directedMessage = new DirectedMessage();
-		UserData userData = new UserData();
-		userData.setLogin(login);
-		userData.setPassword(password);
 
 		directedMessage.setUserData(userData);
 
 		ExpenseData expenseData = new ExpenseData();
-		expenseData.setText(expense.getText());
-		expenseData.setAmount(expense.getAmount());
-		expenseData.setCategory(expense.getCategory());
+		expenseData.setText(text);
+		expenseData.setAmount(amount);
+		expenseData.setCategory(category);
 		
 		directedMessage.setExpenseData(expenseData);
 
@@ -219,8 +148,6 @@ public class ExampleClient implements ActionListener, Runnable  {
 
 		String hostname = args[0];
 		String port = args[1];
-
-<<<<<<< HEAD
 		
 		ExampleClient client = new ExampleClient(hostname, port);
 
@@ -235,30 +162,4 @@ public class ExampleClient implements ActionListener, Runnable  {
 		*/
 	}
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		//running.set(true);
-		running.set(true);
-		while(running.get()) {
-			try { 
-				Thread.sleep(2000);
-				System.out.println("Obtaining data from server...");
-				try {
-					//TODO 
-					System.out.println("TODO");
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
-            } catch (InterruptedException e){ 
-                Thread.currentThread().interrupt();
-                System.out.println("Thread was interrupted, Failed to complete operation");
-            }
-		}
-	}
-		
-	
-	public void stop() {
-		this.running.set(false);
-	}
 }
