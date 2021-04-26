223fa66a8b297301c8f4e57ab91b44da3886667b
Haizea Rodriguez <haizearodriguez@opendeusto.es>
2021-04-26 20:31:53 +0200

    Empty test class created

---
+++ ExpenseTracker/src/main/test/ExampleClientTest.java
@@ -0,0 +1,21 @@
+package es.deusto.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleClientTest {
+    
+    @Rule
+    public ClientDriverRule driver = new ClientDriverRule();
+ 
+    @Test
+    public void trial() {
+    
+        System.out.println("1");
+    }
+
+    
+    
+}
