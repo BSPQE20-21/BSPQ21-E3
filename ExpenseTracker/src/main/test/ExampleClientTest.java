d53381e46d19a59a60c48232f0e852ff20636414
mireyaquintana <47787650+mireyaquintana@users.noreply.github.com>
2021-04-27 17:27:56 +0200

    ExampleClient JUnit test #BSPQ21E3-35

--- ExpenseTracker/src/main/test/ExampleClientTest.java
+++ ExpenseTracker/src/main/test/ExampleClientTest.java
@@ -5,17 +5,48 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ExampleClientTest {
-    
-    @Rule
-    public ClientDriverRule driver = new ClientDriverRule();
- 
-    @Test
-    public void trial() {
-    
-        System.out.println("1");
-    }
+import es.deusto.client.ExampleClient;
+import es.deusto.serialization.ExpenseList;
+import es.deusto.serialization.UserData;
 
+public class ExampleClientTest {
+	
+	ExampleClient exampleClient;
     
+
+	/*
+	 * @Before public void setUp() throws Exception { exampleClient = new
+	 * ExampleClient(hostname, port); exampleClient.storeExpense(userData,
+	 * expenseData); }
+	 * 
+	 * @Test public void testRegisterUser() { ExpenseList expenses = new
+	 * ExpenseList();
+	 * 
+	 * 
+	 * assertEquals(expenses, exampleClient.showExpenses(userData)); }
+	 */
     
+    @Before
+    public void setUp() throws Exception {
+    	
+    	exampleClient = new ExampleClient("127.0.0.1", "8080");
+    	String login = "user";
+    	String password = "12345";
+    	
+    	UserData userExpected = new UserData();
+    	userExpected.setLogin("user");
+    	userExpected.setPassword("12345");
+    	userExpected.setAge(20);
+    	userExpected.setCardNumber("123456789");
+    	userExpected.setExpenseLimit(2000);
+    	
+    }
+    
+    @Test
+    public void testRegisterUser() {
+    	
+    	UserData userDB = exampleClient.validateUser(login, password);
+    	assertEquals(userExpected, userDB);
+    	
+    }
 }
