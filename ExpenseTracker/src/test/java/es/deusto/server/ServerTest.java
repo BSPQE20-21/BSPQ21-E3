9a2c1da6f783eed60b306f521bc2b6c6d81c87fb
mireyaquintana <47787650+mireyaquintana@users.noreply.github.com>
2021-05-01 12:03:18 +0200

    Trying performance testing, but we are having issues #BSPQ21E3-39

--- ExpenseTracker/src/test/java/es/deusto/server/ServerTest.java
+++ ExpenseTracker/src/test/java/es/deusto/server/ServerTest.java
@@ -1,12 +1,37 @@
 package es.deusto.server;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import static org.mockito.Mockito.*;
 
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+
+import es.deusto.serialization.UserData;
+
+@PerfTest(invocations = 5)
+@Required(max = 1200, average = 250)
 public class ServerTest {
+	
+	Server server;
+	UserData user;
 
+	@Before
+    public void setUp() throws Exception {
+		
+		
+		//user = new UserData("user", "12345", "123456789", 20, 2000);
+    	
+		
+	}
+	
 	@Test
+	@PerfTest(invocations = 100, threads = 20)
+	@Required(max = 20000, average = 3000)
 	public void testServer() throws Exception {
-
+		server = new Server();
 	}
 
 	@Test
@@ -21,6 +46,18 @@ public class ServerTest {
 
 	@Test
 	public void testRegisterUser() throws Exception {
+		//server.registerUser(user);
+		
+		//ArgumentCaptor<UserData> userCaptor = ArgumentCaptor.forClass(UserData.class);
+		
+		
+		//verify(server).registerUser(userCaptor.capture());
+		
+		//UserData newUser = userCaptor.getValue();
+		
+		//Assert.assertEquals(user.getLogin(), newUser.getLogin());
+		
+		
 
 	}
 
