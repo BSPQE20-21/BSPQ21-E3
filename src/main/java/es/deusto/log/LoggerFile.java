e0fd61e113b5472956bbf31e4b2b7eb3a286391e
EduRodriguez98 <55583447+EduRodriguez98@users.noreply.github.com>
2021-05-22 12:45:02 +0200

    Added documentation for the LoggerFile.java class

--- src/main/java/es/deusto/log/LoggerFile.java
+++ src/main/java/es/deusto/log/LoggerFile.java
@@ -1,24 +1,34 @@
 package es.deusto.log;
 
-import org.slf4j.*;
 import java.util.logging.*; 
 import java.io.IOException; 
-import javax.swing.*; 
-import java.io.*;
 import java.util.logging.Logger;
+
+/**
+ * This is a logger class that systematically records events in a log to keep track of changes and communication happening in the execution of the program.
+ */
 
 public class LoggerFile {
    
+	/**
+	 * Complete constructor.\n
+	 * @param Logger - composed of body text
+	 * @param fileHandler - writes logging records into a specific file
+	*/
 
     static Logger logger;
     public Handler fileHandler;
     Formatter plainText;
 
     private LoggerFile() throws IOException{
-        //Instance the logger
+    	/**
+    	 * Instantiation of the logger
+    	 */
         logger = Logger.getLogger(LoggerFile.class.getName());
-        //Instance the filehandler
         
+        /**
+    	 * Instantiation of the fileHandler
+    	 */
         fileHandler = new FileHandler("myLog.txt",true);
         //Instance formatter, set formatting and handler
         plainText = new SimpleFormatter();
@@ -26,6 +36,9 @@ public class LoggerFile {
         logger.addHandler(fileHandler);
 
     }
+    /**
+	 * Method that selects specified file to dump logs into; In case the file is not found by the method, or doesn't exit, it will create one.
+	 */
     private static Logger getLogger(){
         if(logger == null){
             try {
@@ -36,6 +49,11 @@ public class LoggerFile {
         }
         return logger;
     }
+    /**
+	 * Method to call the log and write in it categorising the entry. The method receives two inputs:\n
+	 * @param level - used to specify by urgency the category of the log entry. 
+	 * @param msg - the body in form of text of the log
+	 */
     public static void log(Level level, String msg){
         getLogger().log(level, msg);
         //System.out.println(msg);
